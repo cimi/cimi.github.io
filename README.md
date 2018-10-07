2c67aa016b09e1142210cb30eea2b3bd3cec362a
Alex Ciminian <alex.ciminian@gmail.com>
2018-10-08 00:11:37 +0100

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-
-Penrose triangle rendered with Three.js and WebGL.
-
-The technique was inspired from https://codepen.io/Mamboleoo/pen/oepmBM.
-
-Project bootstrapped with Create-react-app.
-https://color.adobe.com/Phaedra-color-theme-1764754/
-
-https://www.shadertoy.com/view/Xd3yDX
---
+++ precache-manifest.169d8442b72bf4107af799f1e2beae25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4148c0041652c7e2a864",
+    "url": "/static/js/main.4148c004.chunk.js"
+  },
+  {
+    "revision": "c6572f3a9feba8d6119e",
+    "url": "/static/js/1.c6572f3a.chunk.js"
+  },
+  {
+    "revision": "4148c0041652c7e2a864",
+    "url": "/static/css/main.34fbeae0.chunk.css"
+  },
+  {
+    "revision": "c295807776534e067c656d345cf11dca",
+    "url": "/index.html"
+  }
+];
