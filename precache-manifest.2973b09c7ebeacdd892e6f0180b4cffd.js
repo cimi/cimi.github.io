ebb6ca214619c3bdf0b832a34cdef087f78efcd9
Alex Ciminian <alexandru.ciminian@datadoghq.com>
2020-10-31 23:32:58 +0000

    Updates

---
+++ precache-manifest.2973b09c7ebeacdd892e6f0180b4cffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83eaa3dfaef4389c2c4e46b3a9635ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e547f7e585a626291911",
+    "url": "/static/css/main.e5b2d5b1.chunk.css"
+  },
+  {
+    "revision": "b16d8480ec888d79783d",
+    "url": "/static/js/2.f5c8a4a2.chunk.js"
+  },
+  {
+    "revision": "e547f7e585a626291911",
+    "url": "/static/js/main.2f0294e3.chunk.js"
+  },
+  {
+    "revision": "787f98d6117fa2021afd",
+    "url": "/static/js/runtime-main.5b255ac7.js"
+  }
+]);
