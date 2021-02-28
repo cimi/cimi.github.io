ff902239b2531dfc76255208022ae076d54db21c
Alex Ciminian <alexandru.ciminian@datadoghq.com>
2021-02-28 21:32:03 +0000

    Use iframes to cycle through different visualisations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import "./index.css";
-import { renderTriangle } from "./triangle";
+// import { renderTriangle } from "./triangle";
 
 import * as serviceWorker from "./serviceWorker";
 
+const gallery = [
+  "https://cimi.io/many-worlds",
+  "https://cimi.io/penrose-tiling",
+  "https://cimi.io/color-automata"
+];
+let idx = 0;
+
 window.onload = () => {
-  renderTriangle();
+  // renderTriangle();
+  const iframe = document.getElementById("gallery");
+  setInterval(() => {
+    iframe.src = gallery[idx];
+    idx = (idx + 1) % gallery.length;
+  }, 10 * 1000);
 };
 
 // ReactDOM.render(<App />, document.getElementById('root'));
